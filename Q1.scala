612d0acdce1f28cb39fd8440e91ae6b526d52013
sohanihansika <136710534+sohanihansika@users.noreply.github.com>
2023-08-29 02:15:24 +0530

    Update Q1.scala

--- Q1.scala
+++ Q1.scala
@@ -1,25 +1,17 @@
-package Labsheet10
-
-import scala.io.StdIn
+package labsheet_10
 
 object Q1 {
-  def main(args: Array[String]): Unit ={
-    print("Enter the list of temperatures in celsius")
-    val InputString = StdIn.readLine()
-    val InputList = InputString.split(",").map(_.trim.toInt).toList
-
-    val Output = convertFahrenheit(InputList)
-    val totalFahrenheit = TotalFahrenheits(Output)
-    val averageFahrenheit = totalFahrenheit / Output.length
-    println(s"Average Fahrenheit: $averageFahrenheit")
-
+  def calculateAverage(input: List[Int]): Double = {
+    val Fahrenheit = input.map(celsius => (celsius * 9 / 5) + 32)
+    val total = Fahrenheit.reduce((total, temp) => total + temp)
+    val average = total / Fahrenheit.length
+    average
   }
 
-  def convertFahrenheit(numbers: List[Int]): List[Double] ={
-    numbers.map(num => (num * 9/5) + 32)
+  def main(args: Array[String]): Unit = {
+    val input = List(0, 10, 20, 30)
+    val average = calculateAverage(input)
+    println(s"Average Fahrenheit temperature: $average")
   }
 
-  def TotalFahrenheits(numbers: List[Double]): Double={
-    numbers.reduce((total, temp) => total + temp)
-  }
 }
